f771bebf5f9ff6dd59660df3495b271d80b43e07
Steve Chen <steve@stevechen.me>
2015-09-26 01:25:32 -0400

    Add search bar to layout

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,13 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    if params[:q]
+      search_term = params[:q]
+      @products = Product.where("name LIKE ?", "%#{search_term}%")
+      # return our filtered list here
+    else
+      @products = Product.all
+    end
   end
 
   # GET /products/1
