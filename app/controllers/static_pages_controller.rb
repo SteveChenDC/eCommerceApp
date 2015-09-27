503c98d328de973fc58fa0b4466e4c00a69fcc9a
Steve Chen <steve@stevechen.me>
2015-09-27 01:35:54 -0400

    Remove ActionMailer code

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,7 +8,6 @@ class StaticPagesController < ApplicationController
   end
 
 
-ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'to@domain.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body.').deliver
 	def thank_you
 		@name = params[:name]
 		@email = params[:email]
