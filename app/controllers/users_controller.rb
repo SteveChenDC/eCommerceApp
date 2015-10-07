e1ab45f6e7c325dc7b63659e2a5c54379a16a57a
Steve Chen <steve@stevechen.me>
2015-10-07 04:14:41 -0400

    Add :email and :password to user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name)
+      params.require(:user).permit(:first_name, :last_name, :email, :encrypted_password)
     end
 end
