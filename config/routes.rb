1ea5d4fd558d7f8b08b35092793f28ae49ee5264
Steve Chen <steve@stevechen.me>
2015-10-10 06:15:23 -0400

    Testing branch

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,6 @@ Rails.application.routes.draw do
 
   post 'static_pages/thank_you'
 
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
