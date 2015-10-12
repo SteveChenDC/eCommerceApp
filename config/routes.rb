b894f785cd9c1cb22419eae815d4ef75e6b5a7c9
Steve Chen <steve@stevechen.me>
2015-10-12 03:47:50 -0400

    Change carousel to 600px

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :new, :create]
   
-  root 'static_pages#about'
+  root 'static_pages#landing_page'
 
   get 'static_pages/about'
 
