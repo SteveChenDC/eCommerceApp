b8f44f04781be5bf240575fe532f61c97d4b0171
Steve Chen <steve@stevechen.me>
2015-10-19 15:49:51 -0400

    Add pagnation to comments and center the view for pagnation links

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,8 @@
 class CommentsController < ApplicationController
 
+  def index
+  end
+
   def create
     @product = Product.find(params[:product_id])
     @comment = @product.comments.new(comment_params)
@@ -14,16 +17,14 @@ class CommentsController < ApplicationController
       end
     end
   end
-    @comment.save
-    redirect_to products_path(@product)
-  end
+
 
   def destroy
   end
 
-private
+  private
 
-  def comment_params
-    params.require(:comment).permit(:user_id, :body, :rating)
-  end
+    def comment_params
+      params.require(:comment).permit(:user_id, :body, :rating)
+    end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,5 @@ validates :body, presence: true
   validates :user, presence: true
   validates :product, presence: true
   validates :rating, numericality: { only_integer: true }
+
 end
