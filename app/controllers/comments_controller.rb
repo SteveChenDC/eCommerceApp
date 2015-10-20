3bd1ca909c3ebaa67de74411d5f9153479ef2623
Steve Chen <steve@stevechen.me>
2015-10-20 17:26:52 -0400

    Add destroy method to Comments Controller, Restrict delete ability to admin, add functional destroy icon in comments view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,6 +20,10 @@ class CommentsController < ApplicationController
 
 
   def destroy
+    @comment = Comment.find(params[:id])
+    product = @comment.product
+    @comment.destroy
+    redirect_to product
   end
 
   private
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,10 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+    end
   end
-
 end
