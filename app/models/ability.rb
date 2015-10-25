b2ab5de0d5cc10f00fea661747aad743324f2a2e
Steve Chen <steve@stevechen.me>
2015-10-25 14:42:29 -0400

    Add functional tests, add user level abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,9 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    if user.admin?
+    if user
+      can :manage, User, id: user.id
+    elsif user.admin?
       can :manage, :all
     else
       can :read, :all
---
+++ spec/controllers/user_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe UsersController, :type => :controller do
+  
+  before do 
+    @user = User.create(:email => "exampleemail@example.com", :password => "12345678")
+    @user2 = User.create(:email => "exampleemail2@example.com", :password => "123456789")
+  end
+
+  describe "GET #show" do
+    context "User is logged in" do
+
+      before do
+        sign_in @user 
+      end
+
+      it "loads correct user details" do
+        get :show, id: @user.id
+        expect(response).to have_http_status(200)
+        expect(assigns(:user)).to eq @user
+      end
+
+      it "redirects to root path" do
+        get :show, id: @user2.id
+        expect(response).to redirect_to(root_path)
+        expect(response).to have_http_status(302)
+      end
+    end
+
+    context "No user logged in" do
+      it "redirects to login" do
+        get :show, id: @user.id
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
+end
