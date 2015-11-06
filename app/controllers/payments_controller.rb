00822671a9a761b4473839ea62ff9233d3b75813
Steve Chen <steve@stevechen.me>
2015-11-06 01:48:49 -0500

    Add payments controller, modify route to POST payments/create, add product_id using hidden field tag to stripe_checkout partial

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,34 @@
+class PaymentsController < ApplicationController
+  Stripe.api_key = "sk_test_C2Zt57LyKyl4s9s9b1wMVEAw"
+  def create
+    # Finds product with the params of :product_id
+    @product = Product.find(params[:product_id])
+    # Refers to current user
+    @user = current_user
+    # Get credit card details submitted by the form
+    token = params[:stripeToken]
+    # Create charge on Stripe's servers - this will charge the user's card
+    begin
+      charge = Stripe::Charge.create(
+        :amount => @product.price, # amount in cents
+        :currency => "usd",
+        :source => token,
+        :description => params[:stripeEmail]
+        )
+      if charge.paid
+        Order.create(
+          :product_id => @product.id,
+          :user_id => current_user,
+          :total => @product.price
+          )
+      end
+
+    rescue Stripe::CardError => e
+      # The card has been declined
+      body = e.json_body
+      err = body[:error]
+      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+    end
+    redirect_to product_path(@product)
+  end
+end
