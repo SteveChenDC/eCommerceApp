21d45152c3a1a6ffa664e1f6c7edbc8a669adc3e
Steve Chen <steve@stevechen.me>
2015-11-26 18:05:54 -0500

    Revert caching changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery_ujs
 //= require angular
 //= require angular-resource
-//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
