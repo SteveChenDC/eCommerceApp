38e9b934a6afdbd0bbc8771b683508d4f9dfeb56
Steve Chen <steve@stevechen.me>
2015-11-26 18:24:57 -0500

    Edit application and devise config

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Testapp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.action_controller.perform_caching = true
-    config.serve_static_assets = true
+    config.serve_static_files = true
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
